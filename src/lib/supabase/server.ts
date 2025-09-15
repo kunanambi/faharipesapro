0b23aa6d04c0fd4cdd45a6cae9238ff4bb415d34
Joseph Kunambi <jayvannyhqemstz@gmail.com>
2025-09-15 16:14:32 +0000

    Try fixing this error: `Runtime Error: Error: Your project's URL and Key

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,9 +1,6 @@
 
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export function createClient() {
   const cookieStore = cookies()
